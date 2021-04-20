e017558023c3ead50df8b07f25787864a5ac1c11
ElderSam <eldersamuel98@gmail.com>
2021-04-20 12:30:09 -0300

    add Lower case in verify costumerExists

--- backend/src/app/controllers/CostumerController.ts
+++ backend/src/app/controllers/CostumerController.ts
@@ -14,7 +14,9 @@ class CostumerController {
         const repository = getRepository(Costumer);
         const { name, phone, birth_date, status } = req.body;
 
-        const costumerExists = await repository.findOne({ where: { name } });
+        const costumerExists = await repository.createQueryBuilder("costumers")
+            .where("LOWER(name) = LOWER(:name)", { name })
+            .getOne();
 
         if(costumerExists) { // implementation of the Singleton design pattern
             return res.status(409).send({ error: 'Já existe um usuário com este nome' }); // status 409: conflict
